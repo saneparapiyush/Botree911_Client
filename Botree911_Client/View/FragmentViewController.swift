f3b6e52d7d1075cfa53bd266778d339a68f9dbdd
saneparapiyush <piyush.sanepara@botreetechnologies.com>
2017-02-10 19:06:09 +0530

    Add SlideMenu, CarbonKit

---
+++ Botree911_Client/View/FragmentViewController.swift
@@ -0,0 +1,35 @@
+//
+//  FragmentViewController.swift
+//  Botree911_Client
+//
+//  Created by piyushMac on 09/02/17.
+//  Copyright © 2017 piyushMac. All rights reserved.
+//
+
+import UIKit
+
+class FragmentViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Botree911_Client/View/ThemeLabel.swift
@@ -0,0 +1,9 @@
+//
+//  ThemeLabel.swift
+//  Botree911_Client
+//
+//  Created by piyushMac on 10/02/17.
+//  Copyright © 2017 piyushMac. All rights reserved.
+//
+
+import Foundation
